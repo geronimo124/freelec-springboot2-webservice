9ed0044c059d94356b1e99187ce17eb22ddfcc2c
tube <geronimo124@hanmail.net>
2020-09-10 23:47:45 +0900

    test code modified

--- src/test/java/com/tube/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/tube/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -27,16 +26,15 @@ public class PostsRepositoryTest {
 
     @Test
     public void 게시글저장_불러오기() {
-
         //given
         String title = "테스트 게시글";
         String content = "테스트 본문";
 
         postsRepository.save(Posts.builder()
-                                .title(title)
-                                .content(content)
-                                .author("geronimo124@hanmail.net")
-                                .build());
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
 
         //when
         List<Posts> postsList = postsRepository.findAll();
@@ -49,25 +47,22 @@ public class PostsRepositoryTest {
 
     @Test
     public void BaseTimeEntity_등록() {
-
         //given
         LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
         postsRepository.save(Posts.builder()
-                                .title("title")
-                                .content("content")
-                                .author("author")
-                                .build());
-
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
         //when
         List<Posts> postsList = postsRepository.findAll();
 
         //then
         Posts posts = postsList.get(0);
 
-        System.out.println(">>>>>> createDate=" + posts.getCreatedDate()+", modifiedDate=" + posts.getModifiedDate());
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
 
         assertThat(posts.getCreatedDate()).isAfter(now);
         assertThat(posts.getModifiedDate()).isAfter(now);
-
     }
 }
